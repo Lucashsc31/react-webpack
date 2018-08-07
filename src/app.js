aacc44c5da0ca35dced2c7ee148b53a57fe42a7b
Lucas Henrique <lucashsc31@gmail.com>
2018-08-07 20:46:33 -0300

    Configurando JSX no babel e source-maps no Webpack

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 let Title = React.createClass({
     render: function(){
-        return React.createElement('h1', null, 'Titulo')
+        return <h1>Titulo</h1>
     }
 })
 
